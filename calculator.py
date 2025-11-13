333ab18e50d2c8ad6474486664f79fed59af9186
nicaniemi <nniemi@ufl.edu>
2025-11-13 13:50:10 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +7,22 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a*b
+def divide(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError:
+        return "Cannot divide by 0"
+def logarithm(a, b):
+    try:
+        return math.log(a, b)
+    except ValueError:
+        return "Error: Cannot log a negative number"
+def exponent(a, b):
+    return a**b
 
 

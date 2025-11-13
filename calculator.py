573c4349070e6feb10ff16c9726b8752f689dfe4
partner 1 <nniemi@ufl.edu>
2025-11-13 14:41:29 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -8,21 +8,21 @@ One function per operation, in order.
 # First example
 def add(a, b): 
     return a + b
-def subtract(a, b):
+def sub(a, b):
     return a - b
-def multiply(a, b):
+def mul(a, b):
     return a*b
-def divide(a, b):
+def div(a, b):
     try:
         return b/a
     except ZeroDivisionError:
         return "Cannot divide by 0"
-def logarithm(a, b):
+def log(a, b):
     try:
         return math.log(a, b)
     except ValueError:
         return "Error: Cannot log a negative number"
-def exponent(a, b):
+def exp(a, b):
     return a**b
 
 

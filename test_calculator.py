1ed9ac29c3b2b8f58107dac76e89635e2685968b
partner1 <nniemi@ufl.edu>
2025-11-13 21:39:31 -0500

    added link to other

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 from calculator import *
-
+#https://github.com/nicaniemi/lab11-NN.git
 class TestCalculator(unittest.TestCase):
     ######## Partner 2
     def test_add(self): # 3 assertions
